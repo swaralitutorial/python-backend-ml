5fb25fd02d9d18790159f9a85ff69df2c4cd72aa
Swarali <swaralimore3@gmail.com>
2026-02-23 22:16:43 +0530

    added integer datatype example

--- 01_basics/datatypes/numerics/01_int.py
+++ 01_basics/datatypes/numerics/01_int.py
@@ -1,15 +1,13 @@
-# Integer Practice
+# Integer Data Type Example
 
-a = 2
-b = 4
+a = 10
+b = 5
 
 print("Value of a:", a)
 print("Value of b:", b)
+print("Type of a:", type(a))
 
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 print("Division:", a / b)
-print("Floor Division:", a // b)
-print("Modulus (Remainder):", a % b)
-print("Power:", a ** b)
